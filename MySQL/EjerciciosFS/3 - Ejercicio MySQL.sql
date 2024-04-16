dab36049fbac424002ca13b56c9e9c9d95e9f452
Pablo83G <128129967+Pablo83G@users.noreply.github.com>
2024-04-16 12:58:18 +0200

    Delete MySQL directory

--- MySQL/EjerciciosFS/3 - Ejercicio MySQL.sql
+++
@@ -1,8 +0,0 @@
-use pvalverde;
-
-SELECT l.titulo, l.anho_publicacion, a.nombre, a.apellidos
-FROM libros AS l
-JOIN autores AS a ON l.id_autor_libro = a.id_autor
-WHERE a.id_autor = 04
-OR (a.id_autor = 06 AND l.titulo = 'Los Godos')
-ORDER BY l.anho_publicacion ASC;
